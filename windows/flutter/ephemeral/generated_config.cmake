a9fd02d5d8eb4d002bc9f37bacb9a73be64625e5
algonacci <86970816+algonacci@users.noreply.github.com>
2021-07-12 22:00:35 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\asus\\Desktop\\flutter_time_tracker" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\asus\\Desktop\\flutter_time_tracker"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\asus\\Desktop\\flutter_time_tracker\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\asus\\Desktop\\flutter_time_tracker"
+  "FLUTTER_TARGET=C:\\Users\\asus\\Desktop\\flutter_time_tracker\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\asus\\Desktop\\flutter_time_tracker\\.dart_tool\\package_config.json"
+)
