da1f970f0fce93020ce7e6c2197313b8ffed5923
algonacci <86970816+algonacci@users.noreply.github.com>
2021-07-15 22:12:07 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter_time_tracker/app/landing_page.dart';
 import 'package:flutter_time_tracker/services/auth.dart';
